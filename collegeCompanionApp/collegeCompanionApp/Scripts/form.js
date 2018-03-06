cc11b9af7848ed1d9e85d996eebcb2160670819f
Rochelle Simpson <rsimpson15@wou.mail.edu>
2018-03-06 07:52:04 -0800

    Commiting to change branches

--- collegeCompanionApp/collegeCompanionApp/Scripts/form.js
+++ collegeCompanionApp/collegeCompanionApp/Scripts/form.js
@@ -1,26 +1,34 @@
-﻿/**
- * Method checks what the values are in School Name and State Name are when called
- * If they are both empty (adding up to zero in length) it returns false
- * Else if returns true.
- */
+﻿console.log("Running form.js!");
+
+// Method calls checkFields when the submit button is pressed
+// If checkFields returns false then it throws the user an error.
+
+function start() {
+    console.log("Submit button clicked!");
+    var oneFilled = checkFields($(this));
+    console.log("oneFilled: " + oneFilled);
+    if (oneFilled === false) {
+        $('#feedbackNoInput').text('Please fill in at least one search criteria.');
+        //var form = $("#feedbackNoInput"); function handleForm(event) { event.preventDefault(); }
+        //form.addEventListener('submit', handleForm);
+        return false;
+    }
+}
+
+
+// Method checks what the values are in School Name and State Name are when called
+// If they are both empty (adding up to zero in length) it returns false
+// Else if returns true.
+
+
 function checkFields(form) {
-    schoolName = document.getElementById("schoolNameInput").trim($(this).val());
-    stateName = document.getElementById("stateInput").value().trim($(this).val());
-    if (schoolName.length + stateName.length === 0) {
+    console.log("Inside checkfields()!");
+    var schoolName = $('#schoolNameInput').val();
+    console.log("School Name: " + schoolName);
+    var stateName = $('#stateInput').val();
+    console.log("State Name: " + stateName);
+    if (schoolName || stateName == null) {
         return false;
     }
     return true;
 }
-
-/**
- * Method calls checkFields when the submit button is pressed
- * If checkFields returns false then it throws the user an error.
- */
-$(function () {
-    $('#errorHandler').on('submit', function () {
-        var oneFilled = checkFields($(this));
-        if (oneFilled == false) {
-            document.getElementById("feedbackNoInput").innerHTML = 'Please fill in at least one search criteria.';
-        };
-    });
-});​
