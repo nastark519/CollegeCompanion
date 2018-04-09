36dcfc70b3c7dbd49994c4418898e9e0e03102e4
Nathan Stark <nstark13@wou.edu>
2018-04-08 23:58:30 -0700

    I need forther clarification on what to do for our test classes

---
+++ collegeCompanionApp/UnitTestCollegeCompanion/TestMethodAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UnitTestCollegeCompanion
+{
+    internal class TestMethodAttribute : Attribute
+    {
+    }
+}
---
+++ collegeCompanionApp/UnitTestCollegeCompanion/UnitTest2.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCollegeCompanion
+{
+    [TestCase]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
