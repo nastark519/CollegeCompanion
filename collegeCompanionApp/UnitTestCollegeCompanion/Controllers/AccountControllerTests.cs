81d4725ae76b033ba265d5cb3c758564d9393496
Nathan Stark <nstark13@wou.edu>
2018-04-09 20:26:07 -0700

    setup nunit tests

---
+++ collegeCompanionApp/UnitTestCollegeCompanion/Controllers/AccountControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using collegeCompanionApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collegeCompanionApp.Controllers.Tests
+{
+    [TestFixture()]
+    public class AccountControllerTests
+    {
+        [Test()]
+        public void VerifyCodeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
