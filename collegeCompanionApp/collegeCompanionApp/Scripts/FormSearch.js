3d2b4d38de5e3531f6115d3ddfa232b2a4553921
daniel_tapia <dtapia12@mail.wou.edu>
2018-05-03 12:58:37 -0700

    Fixed search results form page

--- collegeCompanionApp/collegeCompanionApp/Scripts/FormSearch.js
+++ collegeCompanionApp/collegeCompanionApp/Scripts/FormSearch.js
@@ -3,10 +3,12 @@
 $('#submit').click(start);
 
 function start() {
+    //Clear Message
     $('#feedbackNoInput').empty();
 
     console.log("Submit button clicked!");
 
+    // Get User College Search Input
     var schoolName = $('#schoolNameInput').val();
     var state = $('#stateInput').val();
     var city = $('#cityInput').val();
@@ -15,8 +17,16 @@ function start() {
     var cost = $('#costInput').val();
     var finLimit = $('#finLimitInput').val();
     var acceptRate = $('#acceptRateInput').val();
+    var degree = $('#degreeInput').val();
+    var degreeType = $('#degreeType').val();
 
-    console.log("FinLimit:" + finLimit);
+    if (accreditor === undefined) {
+        accreditor = '';
+    }
+    if (cost === undefined) {
+        cost = '';
+    }
+
     //Get the Financial Limits from the SearchForm
     //Parse the data into an upper and lower limit for searching
     var finBounds;
@@ -26,24 +36,26 @@ function start() {
         finBounds = finLimit.split(" ");
         lowerBound = finBounds[0];
         upperBound = finBounds[1];
+        console.log("FinLimit Given, UpperBound:" + upperBound);
     }
 
-    console.log("FinLimit Given, UpperBound:" + upperBound);
 
+    // Error Message if no state selected
     if (state === null) {
         $('#feedbackNoInput').html("Please select a State!");
     } else {
 
+        // If no ownership selected, defualt all options
         if (ownership === null) {
-            console.log("Ownership is Empty!")
-            ownership = "1,2,3"
+            console.log("Ownership is Empty!");
+            ownership = "1,2,3";
         }
 
+        // If finlimit null, default output 
         if (finLimit === null) {
-            console.log("FinLimit:" + finLimit);
+            console.log("Null FinLimit:" + finLimit);
             finLimit = "";
             upperBound = "";
-            console.log("Fin Limit Empty, UpperBound:" + upperBound);
             lowerBound = "";
         }
 
@@ -51,6 +63,7 @@ function start() {
             $('#feedbackNoInput').html("Please enter your cost per year or select it, not both.");
         }
 
+        // Console Check
         console.log("School Name: " + schoolName);
         console.log("Ownership: " + ownership);
         console.log("City: " + city);
@@ -59,16 +72,19 @@ function start() {
         console.log("Cost:" + cost);
         console.log("Acceptance Rate:" + acceptRate);
 
+        // Add all values into fields
         var fields = "&schoolName=" + schoolName + "&state=" + state + "&city=" + city +
             "&accreditor=" + accreditor + "&ownership=" + ownership + "&lowerBound=" + lowerBound + "&upperBound=" + upperBound + "&cost=" + cost
             + "&acceptanceRate=" + acceptRate;
+
+        // url to Search Results 
         var url = "SearchResults?" + fields;
         url = url.replace(/ /g, "%20"); //replace spaces with '%20'
 
-
         console.log("URL: " + url);
 
-        window.location.href = url;
+        // Returns the URL of the current page
+        //window.location.href = url;
 
     }
 }
